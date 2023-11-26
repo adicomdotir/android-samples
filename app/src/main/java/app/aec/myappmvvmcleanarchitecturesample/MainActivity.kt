abdec84b45377a32d6d42c5dddf457e00bd48fc6
Yashar Panahi <y.panaahi@ravanertebat.com>
2023-11-26 16:07:10 +0330

    Add vm & live data sample

--- app/src/main/java/app/aec/myappmvvmcleanarchitecturesample/MainActivity.kt
+++ app/src/main/java/app/aec/myappmvvmcleanarchitecturesample/MainActivity.kt
@@ -1,12 +1,28 @@
 package app.aec.myappmvvmcleanarchitecturesample
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import app.aec.myappmvvmcleanarchitecturesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        viewModel = ViewModelProvider(this, MainViewModelFactory(0))[MainViewModel::class.java]
+
+        binding.btnClick.setOnClickListener {
+            viewModel.increaseCounter()
+        }
+
+        viewModel.counter.observe(this) {
+            binding.tvMsg.text = it.toString()
+        }
     }
 }
