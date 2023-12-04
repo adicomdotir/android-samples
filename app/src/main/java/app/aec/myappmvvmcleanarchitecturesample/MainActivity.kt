5b3d81b3e7d398047f3b5b204eea00f3dc006526
Yashar Panahi <y.panaahi@ravanertebat.com>
2023-12-04 14:19:03 +0330

    Add work manager sample

--- app/src/main/java/app/aec/myappmvvmcleanarchitecturesample/MainActivity.kt
+++ app/src/main/java/app/aec/myappmvvmcleanarchitecturesample/MainActivity.kt
@@ -1,25 +1,40 @@
 package app.aec.myappmvvmcleanarchitecturesample
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.MutableLiveData
+import androidx.work.Constraints
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import app.aec.myappmvvmcleanarchitecturesample.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val channelID = "ir.adicom.app.channel1"
+    private var notificationManager: NotificationManager? = null
     private lateinit var binding: ActivityMainBinding
-    var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelID, "DemoChannel", "this is a demo")
+
         binding.btnCount.setOnClickListener {
-            binding.tvCount.text = count++.toString()
+            setOneTimeWorkRequest()
         }
 
         val postsMutableLiveData = MutableLiveData<Posts>()
@@ -44,9 +59,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun displayNotification() {
+        val notificationId = 45
+        val tapResultIntent = Intent(this, SecondActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_MUTABLE
+        )
+
+        val notification = NotificationCompat.Builder(this@MainActivity, channelID)
+            .setContentTitle("Demo Title")
+            .setContentText("This is a demo notification")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .build()
+        notificationManager?.notify(notificationId, notification)
+    }
+
+    private fun createNotificationChannel(id: String, name: String, channelDescription: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = channelDescription
+            }
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
+
     private fun downloadUserData() {
         for (i in 1..200000) {
             Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
         }
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val workManager = WorkManager.getInstance(applicationContext)
+        val constraints = Constraints.Builder().setRequiresCharging(true).build()
+
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setConstraints(constraints)
+            .build()
+        workManager.enqueue(uploadRequest)
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id).observe(this) {
+                Log.e("TAG", it.state.name)
+            }
     }
 }
---
+++ app/src/main/java/app/aec/myappmvvmcleanarchitecturesample/UploadWorker.kt
@@ -0,0 +1,19 @@
+package app.aec.myappmvvmcleanarchitecturesample
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class UploadWorker(context: Context, params: WorkerParameters): Worker(context, params) {
+    override fun doWork(): Result {
+        try {
+            for (i in 0..600) {
+                Log.i("TAG", "Uploading $i")
+            }
+            return Result.success()
+        } catch (e: Exception) {
+            return Result.failure()
+        }
+    }
+}
